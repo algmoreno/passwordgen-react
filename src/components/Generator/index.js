205aa04c8bc1bb08da2426cab6c3dd33b276109b
algmoreno <alg.moreno00@gmail.com>
2021-11-03 11:31:00 -0700

    form not rendering

--- src/components/Generator/index.js
+++ src/components/Generator/index.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-function Generator() {
+const Generator = () => {
 
-  var generatePassword = () => {
+  function generatePassword() {
+    console.log("hello")
+    
+    let message = 'hi there'
 
+    return(
+      <div>{message}</div>
+    )
+    
   }
 
-  var writePassword = () => {
-    
+  function writePassword() {
+
   }
 
   return (
     <div class='container'>
-      <div class='row align-items-end'>
-        <div class='col-3'></div>
-        <div class='col-6'>
+      <div className='row align-items-end'>
+        <div className='col-3'></div>
+        <div className='col-6'>
           <Card className="text-center gen-card">
             <Card.Header className='gen-card-header'>Click Go to Start</Card.Header>
             <Card.Body>
               <Card.Text className='password-text'>
-                {writePassword}
+                {writePassword()}
               </Card.Text>
               <Button onClick={generatePassword} variant="primary">Go</Button>
             </Card.Body>
           </Card>
         </div>
-        <div class= 'col-3'></div>
+        <div className='col-3'></div>
       </div>
     </div>
   )
